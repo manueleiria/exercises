565d8719cb48d3075dfd705545db1e24a22de232
Manuel Leiria <manueleiria@gmail.com>
2016-04-15 18:34:53 +0100

    2nd chapter exercises completed

---
+++ think_java/chapter_2/exercise_2.1.java
@@ -0,0 +1,5 @@
+/*”Stump the Chump”:
+
+Start with a program that compiles and runs correctly. One player turns
+away while the other player adds an error to the program. Then the first
+player tries to find and fix the error. You get two points if you find the error without compiling the program, one point if you find it using the compiler, and your opponent gets a point if you don’t find it.*/
