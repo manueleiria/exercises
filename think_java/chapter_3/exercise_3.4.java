e958d2cac817280928e7bd49868ec0133aa0d721
Manuel Leiria <manueleiria@gmail.com>
2016-04-15 21:39:06 +0100

    Corrected naming, finished chapter 3 exercises

---
+++ think_java/chapter_3/exercise_3.4.java
@@ -0,0 +1,19 @@
+class Date {
+
+  public static void printAmerican(String day, String month, int date, int year) {
+    System.out.println("American format:");
+    System.out.println(day + ", " + month + " " + date + ", " + year);
+  }
+
+  public static void printEuropean(String day, int date, String month, int year) {
+    System.out.println("European format:");
+    System.out.println(day + " " + date + " " + month + ", " + year);
+  }
+
+  public static void main(String[] args) {
+    printAmerican("Friday", "March", 15, 2016);
+    System.out.println();
+    printEuropean("Friday", 15, "March", 2016);
+  }
+
+}
