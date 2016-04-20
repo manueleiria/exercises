0a363338547aba6a718e3f039beb0b10d652c952
Manuel Leiria <manueleiria@gmail.com>
2016-04-20 01:05:46 +0100

    Finished exercise 7.2

--- think_java/chapter_7/exercise_7.2.java
+++ think_java/chapter_7/exercise_7.2.java
@@ -1,10 +1,17 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println(squareRoot(9.0));
+    System.out.printf("%.4f\n", squareRoot(12));
   }
-  public static void squareRoot(double a) {
-    double x = a / 2;
-    x = (x + a/x) / 2;
+  public static double squareRoot(double a) {
+    double x = a / 2.0;
+    double i = 1.0;
+    while (i > 0.0001) {
+      i = Math.abs(x - rootCheck(a, x));
+      x = rootCheck(a, x);
+    }
     return x;
+  }
+  public static double rootCheck(double a, double x) {
+    return (x + a / x) / 2.0;
   }
 }
