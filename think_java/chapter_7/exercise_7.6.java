f31efdb04f54e8a702c9e8936d0999134f839a69
Manuel Leiria <manueleiria@gmail.com>
2016-04-21 01:03:24 +0100

    Updated exercise 7.6

--- think_java/chapter_7/exercise_7.6.java
+++ think_java/chapter_7/exercise_7.6.java
@@ -1,22 +1,8 @@
 class Main {
   public static void main(String[] args) {
-    for (double i = 0.1; i <= 100; i = i * 10) {
-      check(i);
-    }
     for (double i = -0.1; i >= -100; i = i * 10) {
       System.out.println(i + " \t" + gauss(i, 16.0) + "    \t" + Math.exp(i));
     }
-  }
-  public static double myexp(double x, double n) {
-    double result = 1.0;
-    double numenator = x;
-    double denominator = 1.0;
-    for (double i = 1; i <= n; i++) {
-      numenator = numenator * x;
-      denominator = denominator * i;
-      result = result + numenator / denominator;
-    }
-    return result;
   }
   public static double gauss(double x, double n) {
     double result = 1.0;
@@ -30,8 +16,5 @@ class Main {
       result = result + prefix * numenator / denominator;
     }
     return result;
-  }
-  public static void check(double x) {
-    System.out.println(x + " \t" + myexp(x, 16.0) + "    \t" + Math.exp(x));
   }
 }
