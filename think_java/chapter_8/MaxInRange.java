6ce3e48494a06a9268c6e94d84387c016cba647f
Manuel Leiria <manueleiria@gmail.com>
2016-05-04 10:09:53 +0100

    further corrections

--- think_java/chapter_8/MaxInRange.java
+++ think_java/chapter_8/MaxInRange.java
@@ -58,7 +58,7 @@ class MaxInRange {
   public static int[] createIntArr(int size) {
     int[] arr = new int[size + 1];
     for (int i = 0; i <= size; i++) {
-      arr[i] = (int)(Math.random() * (size - 0));
+      arr[i] = (int)(Math.random() * size);
     }
     return arr;
   }
